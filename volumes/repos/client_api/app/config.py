04d06684fb9aa8ff4479df6fb518d1aee3ca3f48
Chukky Nze <chukkynze@gmail.com>
2024-08-27 13:30:56 -0700

    Integrated pydantic based environment configs. Updated main.py

--- volumes/repos/client_api/app/config.py
+++ volumes/repos/client_api/app/config.py
@@ -1,5 +1,78 @@
-from dynaconf import Dynaconf  # type: ignore
+from pydantic_settings import BaseSettings
+from pydantic.types import SecretStr
+from enum import Enum, IntEnum
 
-settings = Dynaconf(
-    settings_files=[".env"],
-)
+class AppEnvironmentEnum(str, Enum):
+    development = 'development'
+    dev = 'development'
+    testing = 'testing'
+    test = 'testing'
+    acceptance = 'acceptance'
+    qa = 'acceptance'
+    production = 'production'
+    prod = 'production'
+
+class AppSettings(BaseSettings):
+    APP_ENV: AppEnvironmentEnum = AppEnvironmentEnum.dev
+    APP_VERSION: str
+    APP_PROJECT_NAME: str
+    APP_PROJECT_DESCRIPTION: str
+
+    MYSQLDB_DRIVERNAME: str
+    MYSQLDB_USERNAME: str
+    MYSQLDB_PASSWORD: SecretStr
+    MYSQLDB_HOST: str
+    MYSQLDB_PORT: int
+    MYSQLDB_DATABASE: str
+    MYSQLDB_ECHO_LOG_LEVEL: str
+    MYSQLDB_FUTURE: bool
+    MYSQLDB_SESSION_AUTOCOMMIT: bool
+    MYSQLDB_SESSION_AUTOFLUSH: bool
+
+    POSTGRESDB_DRIVERNAME: str
+    POSTGRESDB_USERNAME: str
+    POSTGRESDB_PASSWORD: SecretStr
+    POSTGRESDB_HOST: str
+    POSTGRESDB_PORT: int
+    POSTGRESDB_DATABASE: str
+    POSTGRESDB_ECHO_LOG_LEVEL: str
+    POSTGRESDB_FUTURE: bool
+    POSTGRESDB_SESSION_AUTOCOMMIT: bool
+    POSTGRESDB_SESSION_AUTOFLUSH: bool
+
+    REDIS_CACHE_DRIVERNAME: str
+    REDIS_CACHE_USERNAME: str
+    REDIS_CACHE_PASSWORD: SecretStr
+    REDIS_CACHE_HOST: str
+    REDIS_CACHE_PORT: int
+    REDIS_CACHE_PRIMARY_DB: int
+
+    REDIS_SEARCH_DRIVERNAME: str
+    REDIS_SEARCH_USERNAME: str
+    REDIS_SEARCH_PASSWORD: SecretStr
+    REDIS_SEARCH_HOST: str
+    REDIS_SEARCH_PORT: int
+    REDIS_SEARCH_PRIMARY_DB: int
+
+    REDIS_DOC_DRIVERNAME: str
+    REDIS_DOC_USERNAME: str
+    REDIS_DOC_PASSWORD: SecretStr
+    REDIS_DOC_HOST: str
+    REDIS_DOC_PORT: int
+    REDIS_DOC_PRIMARY_DB: int
+
+    REDIS_MONITOR_DRIVERNAME: str
+    REDIS_MONITOR_USERNAME: str
+    REDIS_MONITOR_PASSWORD: SecretStr
+    REDIS_MONITOR_HOST: str
+    REDIS_MONITOR_PORT: int
+    REDIS_MONITOR_PRIMARY_DB: int
+
+    REDIS_AI_DRIVERNAME: str
+    REDIS_AI_USERNAME: str
+    REDIS_AI_PASSWORD: SecretStr
+    REDIS_AI_HOST: str
+    REDIS_AI_PORT: int
+    REDIS_AI_PRIMARY_DB: int
+
+config = AppSettings(_env_file="./.env")
--- volumes/repos/client_api/app/main.py
+++ volumes/repos/client_api/app/main.py
@@ -1,33 +1,73 @@
+import logging
+import os
 from fastapi import FastAPI, Depends, status
 from app.dependencies import get_token_header
-from app.config import settings
+from app.config import config
+from app.log.loggers.app_logger import get_app_logger
 from app.routers import PostsRouter
 from app.exceptions.AppExceptionHandlers import add_app_exception_handlers
+from app.exceptions.data.AppExceptions import add_data_exception_handlers
 
+os.environ["REDIS_OM_URL"] = "redis://client-redis-stack:6379"
 app = FastAPI(
-    title="Awesome API", # settings.PROJECT_NAME,
-    description="This API does awesome things",
+    title=config.APP_PROJECT_NAME,
+    description=config.APP_PROJECT_DESCRIPTION,
     dependencies=[Depends(get_token_header)]
 )
 
+# Logging
+log = get_app_logger()
+
 # Exception Handlers
 add_app_exception_handlers(app)
+add_data_exception_handlers(app)
 
 # Routes
 app.include_router(PostsRouter.router)
 
-@app.get(
-    "/",
-    status_code=status.HTTP_200_OK,
-)
+
+@app.get("/", status_code=status.HTTP_200_OK)
 async def root():
+    log.info("Root endpoint hit.")
+
+    output_data = {
+        "company": "AcademyStack LLC",
+        "author": "Chukwuma Nze",
+        "description": config.APP_PROJECT_DESCRIPTION,
+    }
+
+    if config.APP_ENV in ["development", "dev"]:
+        output_data['settings'] = config.dict()
+
+    output_meta = {
+        "timestamp": "",
+    }
+
+    output = {
+        "status": True,
+        "message": f"{config.APP_PROJECT_NAME} is up and running.",
+        "data": output_data,
+        "meta": output_meta,
+    }
+
+    return output
+
+# @app.on_event("startup")
+# async def startup_event():
+#     logger.info("Opening mols bakery...")
+#     app.state.redis = await init_redis_pool()
+#     app.state.mols_repo = MoleculesRepository(app.state.redis)
+#
+#
+# @app.on_event("shutdown")
+# async def shutdown_event():
+#     logger.info("Closing mols bakery...")
+#     await app.state.redis.close()
+
+
+@app.get("/health-check", status_code=status.HTTP_200_OK)
+async def health_check():
     return {
         "status": True,
-        "message": "Hello World.",
-        "data": {
-            "company": "AcademyStack LLC",
-            "author": "Chukwuma Nze",
-            "version": 0.00,
-        },
-        "settings": settings,
+        "message": "But Jesus looked at them and said, “With man this is impossible, but with God all things are possible."
     }
