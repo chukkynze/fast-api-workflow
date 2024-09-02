6a4fe0292b4d7e1ff44ffddb7cb1c2510fe60c1e
Chukky Nze <chukkynze@gmail.com>
2024-09-02 06:45:41 -0700

    An intro to seeder integration

---
+++ volumes/repos/client_api/app/database/seeders/seeder.py
@@ -0,0 +1,4 @@
+import sys
+env = sys.argv[1]
+
+print(f'seeding for {env}')
--- volumes/repos/client_api/build.sh
+++ volumes/repos/client_api/build.sh
@@ -6,3 +6,8 @@ pwd
 alembic -n clientdb upgrade head
 alembic -n customerdb upgrade head
 #alembic -n clientcache upgrade head
+
+# Seeders
+cd app/database/seeders || exit
+pwd
+python seeder.py test
